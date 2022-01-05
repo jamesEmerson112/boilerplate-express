8d78fd99755baaa0b7748b9d2e0a3efa3e4b75b2
James Emerson (An Vo) <vo.an.2503@gmail.com>
2022-01-04 16:51:39 -0800

    learning more about sending out json

--- myApp.js
+++ myApp.js
@@ -1,3 +1,4 @@
+const { response } = require('express');
 var express = require('express');
 var app = express();
 
@@ -8,15 +9,31 @@ var app = express();
 //   });
 
 absolutelyPath = __dirname + "/views/index.html";
+app.use(function middlewareLogger(req, res, next){
+    var log = req.method + " " + req.path + " - " + req.ip;
+    console.log(log);
+    next();
+})
 
+app.get('/now', function middlewareNow(req, res, next){
+    var time = new Date().toString();
+    req.time = time;
+    console.log(req.time);
+    next();
+    }, function(req, res) 
+    {
+        res.json({time: req.time});    
+    }
+)
 // Normal usage
 // app.use(express.static(__dirname + "/public"));
 
 // Assets at the /public route
 app.use("/public", express.static(__dirname + "/public"));
-// app.get("/", function(req, res) {
-//         res.sendFile(absolutelyPath);
-//       });
+app.get("/", function(req, res) {
+        res.sendFile(absolutelyPath);
+      });
+      
 
 app.get("/json", function(req, res) {
     if(process.env.MESSAGE_STYLE === "uppercase")
@@ -25,8 +42,17 @@ app.get("/json", function(req, res) {
         res.json({"message": "Hello json"})
 })
 
+app.get("/:word/echo", function(req,res){
+    const word = req.params.word;
+    res.json({echo: word});
+})
 
-
+// app.get("/:word/echo", (req, res) => {
+//     const { word } = req.params;
+//     res.json({
+//       echo: word
+//     });
+//   });
 
 
 
